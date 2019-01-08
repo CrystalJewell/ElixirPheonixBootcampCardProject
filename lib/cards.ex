3d9636282049190d2a54d666fbe33dc54232d8cc
Crystal Adkins <CrystalAdkins09@gmail.com>
2019-01-08 13:44:40 -0500

    Working on card project from Elixir and Phoenix bootcamp from Udemy

---
+++ lib/cards.ex
@@ -0,0 +1,57 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Creates deck of cards
+
+  ## Examples
+
+  """
+
+  def create_deck do
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
+
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  @doc """
+  shuffles deck of cards that is passed into it using Enum.shuffle()
+  """
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+end
